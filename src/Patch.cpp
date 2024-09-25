2caf0d49cedbdc51658ef736f9ae01150e8c8b49
Kjifs <tbartle92@gmail.com>
2024-09-25 11:30:25 -0400

    Add support for renaming additional forms

--- src/Patch.cpp
+++ src/Patch.cpp
@@ -31,7 +31,7 @@ namespace Patch {
             for(auto& entry : value) {
                 auto from = entry.at(0).template get<string>();
                 auto to =  entry.at(1).template get<string>();
-                logger::info("Entry: {} -> {}", from, to);
+                logger::info("{} Entry: {} -> {}", key, from, to);
                 renames[key].push_back(std::make_pair(from,to));
             }
         }
@@ -55,13 +55,15 @@ namespace Patch {
     void renameAll(string type) {
         const auto HANDLER = RE::TESDataHandler::GetSingleton();
         auto things = HANDLER->GetFormArray<T>();
-        logger::info("Found {} entries of type {}", things.size(), type);
-        for (auto * thing : things) {
-            if (!thing) {
-                logger::info("Thing was null");
-                continue;
+        if (renames.contains(type)) {
+            logger::info("Found {} entries of type {}", things.size(), type);
+            for (auto * thing : things) {
+                if (!thing) {
+                    logger::info("Thing was null");
+                    continue;
+                }
+                rename(thing,type);
             }
-            rename(thing,type);
         }
     }
 
@@ -71,6 +73,13 @@ namespace Patch {
         renameAll<RE::TESObjectWEAP>("Weapon");
         renameAll<RE::IngredientItem>("Ingredient");
         renameAll<RE::AlchemyItem>("Ingestible");
+        renameAll<RE::TESObjectACTI>("Activator");
+        renameAll<RE::TESFlora>("Flora");
+        renameAll<RE::TESObjectMISC>("Misc");
+        renameAll<RE::TESFurniture>("Furniture");
+        renameAll<RE::TESObjectDOOR>("Door");
+        renameAll<RE::TESObjectCONT>("Container");
+
 
     }
 }
